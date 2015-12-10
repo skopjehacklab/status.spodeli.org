820d90807db3c626d82449f40a8da37c00f74dee
Goce Mitevski <goce.mitevski@gmail.com>
2015-12-10 03:08:39 +0100

    switch from toggleClass to addClass and modify how classes are added/removed

--- js/status.js
+++ js/status.js
@@ -4,6 +4,7 @@ var INFLUXDB_URL = "https://db.softver.org.mk/influxdb/query";
 var INFLUXDB_DBNAME = "status";
 function updateStatus() {
     var influxdbQuery = $('#status').attr('data-influx-query');
+    var status_container = $("#status").parent().parent();
     $.ajax({
         url: INFLUXDB_URL,
         type: 'GET',
@@ -24,13 +25,16 @@ function updateStatus() {
             }
 
             otvoren = secondsToString(otvoren / 1000);
+
+            status_container.removeClass('panel-open panel-closed');
+
             if (vrednosti[vrednosti.length - 1][1] === "CLOSED") {
                 $("#status").text("Затворен");
-                $("#status").parent().parent().toggleClass('panel-closed');
+                status_container.addClass('panel-closed');
                 $("#status-time").text("веќе " + otvoren);
             } else {
                 $("#status").text("Отворен");
-                $("#status").parent().parent().toggleClass('panel-open');
+                status_container.addClass('panel-open');
                 $("#status-time").text("пред " + otvoren);
             }
         }
@@ -39,6 +43,7 @@ function updateStatus() {
 
 function updateDevices() {
     var influxdbQuery = $('#currentDevices').attr('data-influx-query');
+    var devices_container = $("#currentDevices").parent().parent();
     $.ajax({
         url: INFLUXDB_URL,
         type: 'GET',
@@ -47,22 +52,25 @@ function updateDevices() {
         success: function (response) {
             var logged_devices = response.results[0].series[0].values[0][1];
             var total_devices = response.results[0].series[0].values[0][2];
+
+            devices_container.removeClass('panel-danger panel-success');
+
             switch (true) {
                 case (logged_devices === 0):
                 {
-                    $("#currentDevices").parent().parent().toggleClass('panel-danger');
+                    devices_container.addClass('panel-danger');
                     str_najaveni = "најавени";
                     break;
                 }
                 case (logged_devices % 10 === 1):
                 {
-                    $("#currentDevices").parent().parent().toggleClass('panel-success');
+                    devices_container.addClass('panel-success');
                     str_najaveni = "најавен";
                     break;
                 }
                 case (logged_devices > 0 || logged_devices === 11):
                 {
-                    $("#currentDevices").parent().parent().toggleClass('panel-success');
+                    devices_container.addClass('panel-success');
                     str_najaveni = "најавени";
                     break;
                 }
@@ -95,7 +103,7 @@ function secondsToString(seconds) {
 }
 
 $(document).ready(function () {
-    
+
     // First update
     updateStatus();
     updateDevices();
