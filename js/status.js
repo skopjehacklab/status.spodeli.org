1541b08e71e91ccc41656cc9f5090e95b7d2957e
Goce Mitevski <goce.mitevski@keitaro.com>
2017-11-07 18:11:57 +0100

    Decrease tumblr photo-url size

--- js/status.js
+++ js/status.js
@@ -177,7 +177,9 @@ function populateTumblr(tumblr_id) {
       activeClass = '';
     }
 
-    $(tumblr_id.selector).find(".carousel-inner").append('<div class="item ' + activeClass + '"><img src="' + item['photo-url-1280'] + '" ></div>');
+    console.log(item);
+
+    $(tumblr_id.selector).find(".carousel-inner").append('<div class="item ' + activeClass + '"><img src="' + item['photo-url-500'] + '" ></div>');
   });
 }
 
