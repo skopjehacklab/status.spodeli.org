26bff43eb195c56c9db3fb3cb77c390b658e7bbb
Goce Mitevski <goce.mitevski@keitaro.com>
2018-01-20 21:31:20 +0100

    Add minify-js task

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var less = require('gulp-less');
 var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 // var sourcemaps = require('gulp-sourcemaps'); - Uncomment when developing
-// var rename = require('gulp-rename');
 
 // The default Gulp.js task
-gulp.task('default', ['bootstrap-fonts', 'bootstrap-js', 'jquery', 'jquery-flot', 'less', 'watch']);
+gulp.task('default', ['bootstrap-fonts', 'bootstrap-js', 'jquery', 'jquery-flot', 'minify-js', 'less', 'watch']);
 
 // Rebuild CSS from LESS
 gulp.task('less', function () {
@@ -43,6 +43,14 @@ gulp.task('jquery-flot', function () {
         .pipe(uglify())
         .pipe(gulp.dest('js/flot'));
 });
+
+// Minify custom JS assets
+gulp.task('minify-js', function () {
+    return gulp.src(['js/plot.js', 'js/status.js'])
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('js'));
+})
 
 // Copy Bootstrap font files in assets/fonts
 gulp.task('bootstrap-fonts', function () {
